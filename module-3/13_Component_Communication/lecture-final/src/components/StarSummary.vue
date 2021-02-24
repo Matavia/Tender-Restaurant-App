34935d940731fe90f4ae47da7907181d1f710c49
Andrew McGuier <andrew.mcguier@techelevator.com>
2021-02-24 12:21:36 -0500

    Day 53: Component Communication - Lecture Final

---
+++ module-3/13_Component_Communication/lecture-final/src/components/StarSummary.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="well">
+    <span class="amount" v-on:click="updateFilter()">{{ numberOfReviews }}</span>
+    {{ rating }} Star Review{{ numberOfReviews === 1 ? '' : 's' }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "star-summary",
+  props: ["rating"],
+  methods: {
+    updateFilter() {
+      const ratingNum = parseInt(this.rating);
+      this.$store.commit('UPDATE_FILTER', ratingNum );
+    }
+  },
+  computed: {
+    numberOfReviews() {
+      const reviews = this.$store.state.reviews;
+      return reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === parseInt(this.rating) ? 1 : 0);
+      }, 0);
+    }
+  }
+};
+</script>
+
