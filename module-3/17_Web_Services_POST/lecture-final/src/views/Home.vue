0b96d54219494f6f4a1fc28965089c2fe602f00a
Andrew McGuier <andrew.mcguier@techelevator.com>
2021-03-02 12:22:23 -0500

    Day 57: Web Services POST - Lecture Final

---
+++ module-3/17_Web_Services_POST/lecture-final/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  created() {
+    if(this.$store.state.boards.length > 0){
+      const boardId = this.$store.state.boards[0].id;
+      this.$router.push({name:'Board', params: {id: boardId}});
+    }
+  }
+};
+</script>
